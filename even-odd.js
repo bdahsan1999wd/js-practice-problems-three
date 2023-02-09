35c02d446ffbbc445beea4560d5f49ee08060d3a
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:32:14 +0600

    even-odd added

---
+++ even-odd.js
@@ -0,0 +1,24 @@
+/* 
+1. Loop variable
+2. Condition inside while
+3.Loop body
+4.Change the Loop variable ---> important !!!
+*/
+
+/* ---------------------even number------------------ */
+
+var number = 0;
+while(number <= 100){
+    console.log(number);
+    // number = number + 2;
+    number+=2;
+}
+
+/* ---------------------odd number-------------------- */
+
+var number = 1;
+while(number <= 100){
+    console.log(number);
+    // number = number + 2;
+    number+=2;
+}
