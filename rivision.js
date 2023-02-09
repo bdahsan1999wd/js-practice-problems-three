15008fe27469ec9a28ea9fa0089f049ef239b62e
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:35:19 +0600

    rivision added

---
+++ rivision.js
@@ -0,0 +1,29 @@
+// string type variable
+var bottleColor = 'white';
+
+// number type variable
+var phonePrice = 12000;
+
+// boolean type variable
+var isExpensive = true;
+
+// array
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+var item2 = tableItems[2];
+tableItems[3] = 'mobile';
+var paperIndex = tableItems.indexOf('paper');
+
+// push  pop
+
+
+// --------------------conditionals---------------------
+
+if(tableItems.length > 3){
+
+}
+else if (tableItems[2] == 'mouse'){
+
+}
+else{
+    
+}
