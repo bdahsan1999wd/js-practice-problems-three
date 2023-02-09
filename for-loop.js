6414c7128770ba52d0bede03b58268770e119ae1
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:29:52 +0600

    for loop added

---
+++ for-loop.js
@@ -0,0 +1,40 @@
+/* 
+1. Loop variable
+2. Condition inside while
+3.Loop body
+4.Change the Loop variable !!!
+*/
+
+/*---------- Normal version of for loop ----------*/
+
+for(var roastGiven = 1; roastGiven < 5 ; roastGiven++){
+    console.log('Roast Den...');
+    console.log(roastGiven);
+}
+
+/*---------- simple version of for loop ----------*/ 
+
+for(var i = 1; i <= 20; i++){
+    console.log('Roast Den, Please !!!');
+    console.log(i);
+}
+
+/* -------------------------even and odd number------------------------- */
+
+for(var i = 0; i <= 20; i+=2){
+    console.log(i);
+    console.log('even number');
+}
+
+for(var i = 1; i <= 20; i+=2){
+    console.log(i);
+    console.log('odd number...');
+}
+
+/* 
+
+Alt + Shift + A = Different Comment
+
+Ctrl + C = Stop Terminal Loop !!!
+
+*/
