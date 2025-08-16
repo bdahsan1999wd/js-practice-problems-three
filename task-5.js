4816fab8706c27039fbfefd79e01d63f442d6654
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-16 23:53:15 +0600

    task-5 added

---
+++ task-5.js
@@ -0,0 +1,11 @@
+// 1. Create two arrays
+let colors = ["red", "blue", "green"];
+let shapes = ["circle", "square", "triangle"];
+
+// 2. Use concat method to combine arrays
+let combinedArray = colors.concat(shapes);
+
+// 3. Print both original arrays and combined array
+console.log(colors);
+console.log(shapes);
+console.log(combinedArray);
