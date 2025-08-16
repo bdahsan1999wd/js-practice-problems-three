6e3ce1f2fcead3f39fc1e6aba63fa1c49f3bbcec
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-16 23:45:19 +0600

    task-2 added

---
+++ task-2.js
@@ -0,0 +1,14 @@
+// 1. Declare an array of 3 tourist destinations
+let touristPlaces = ["Cox's Bazar", "Sajek Valley", "Sundarbans"];
+
+// 2. Add a new tourist destination
+touristPlaces.push("Saint Martin");
+
+// 3. Add two more to your array
+touristPlaces.push("Kuakata", "Rangamati");
+
+// 4. Remove the last tourist destination
+touristPlaces.pop();
+
+// 5. Display the final array
+console.log(touristPlaces);
