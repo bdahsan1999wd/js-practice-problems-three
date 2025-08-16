2b4f61e25ba676c01dc2ca68c17352485a1bdbae
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-16 23:27:21 +0600

    task-1 added

---
+++ task-1.js
@@ -0,0 +1,11 @@
+// 1. Declare an array with 5 fruits
+let fruits = ["mango", "apple", "banana", "orange", "lichi"];
+
+// 2. Console log the 3rd index element
+console.log(fruits[3]);
+
+// 3. Change the value of the 2nd index element to jambura
+fruits[2] = "jambura";
+
+// 4. Console log the final array
+console.log(fruits);
