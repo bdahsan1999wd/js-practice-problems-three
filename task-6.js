d27181402cf553292cda68d43217c1c50b8cad70
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-17 18:48:21 +0600

    task-6 added

---
+++ task-6.js
@@ -0,0 +1,11 @@
+// 1. Declare an array of 5 colors
+let colors = ["red", "blue", "green", "yellow", "purple"];
+
+// 2. Use join method to combine all elements into a string separated by commas
+let colorString = colors.join(", ");
+
+// 3. Print the result
+console.log(colorString); // "red, blue, green, yellow, purple"
+
+// 4. Print the original array to show it is not changed
+console.log(colors);
