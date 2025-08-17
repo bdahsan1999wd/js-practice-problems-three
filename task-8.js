9c88aab131614013e5fdf1ef38910adb16fc8232
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-17 19:04:56 +0600

    task-8 added

---
+++ task-8.js
@@ -0,0 +1,11 @@
+// 1. Create an array of 3 programming languages
+let languages = ["JavaScript", "Python", "C++"];
+
+// 2. Use the unshift method to add one new language at the beginning
+let newLength = languages.unshift("Java");
+
+// 3. Print the new length of the array
+console.log(newLength);
+
+// 4. Print the updated array
+console.log(languages);
