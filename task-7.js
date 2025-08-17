6d5c901e37ba07bec307c08ef9c4555d7cc65c12
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-17 19:01:00 +0600

    task-7 added

---
+++ task-7.js
@@ -0,0 +1,11 @@
+// 1. Create an array of 4 friends' names
+let friends = ["Ahsan", "Rafi", "Nayeem", "Sakib"];
+
+// 2. Use the shift method to remove the first element
+let removedFriend = friends.shift();
+
+// 3. Print which element was removed
+console.log(removedFriend);
+
+// 4. Print the updated array
+console.log(friends);
